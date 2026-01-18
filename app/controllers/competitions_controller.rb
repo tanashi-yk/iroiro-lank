06b4851ce9135956903278d708f8367b07d332e5
tanashi-yk <yktn0817@gmail.com>
2026-01-19 02:11:27 +0900

    あと少し

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,27 +1,26 @@
 class CompetitionsController < ApplicationController
+  def create
+    count = Competition.count + 1
+    @competition = Competition.create!(name: "第#{count}回イベント")
+    # 修正：作成した大会のIDをURLに含めて一覧へ飛ばす
+    redirect_to predictions_path(competition_id: @competition.id)
+  end
+
+  # --- edit, update は以前のままでOK ---
   def edit
     @competition = Competition.find(params[:id])
   end
 
   def update
     @competition = Competition.find(params[:id])
-    
-    # フォームから送られてくるデータは {"0"=>"#FF0000", "1"=>"#0000FF", ...} という形
     raw_colors = params[:competition][:answer_colors]
-    
     if raw_colors.present?
-      # 0から5までのキーを順番に取得して、綺麗な配列 ["#FF0000", "#0000FF", ...] に変換
       formatted_colors = (0..5).map { |i| raw_colors[i.to_s] }
-      
       if @competition.update(answer_colors: formatted_colors)
-        # 保存に成功したら、みんなの予想一覧（Index）へ戻る
-        redirect_to predictions_path, notice: "正解を更新しました！"
+        redirect_to predictions_path(competition_id: @competition.id), notice: "正解を更新しました！"
       else
-        render :edit, status: :unprocessable_entity
+        render :edit
       end
-    else
-      # 何も選ばれなかった場合など
-      render :edit, status: :unprocessable_entity
     end
   end
 end
--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,30 +1,31 @@
 class PredictionsController < ApplicationController
   def index
-    # 最新の大会を取得し、ボタンのリンク先や正答判定に使う
-    @competition = Competition.last
+    # URLの competition_id を優先し、なければ最新の大会を表示
+    @competition = Competition.find_by(id: params[:competition_id]) || Competition.last
     
     if @competition
-      # 大会に紐づく予想を取得
+      # その大会に紐づく予想だけを取得
       @predictions = @competition.predictions.includes(:ranking_items)
     else
-      # 大会が一つもない場合、エラー回避のために作成
-      @competition = Competition.create!(name: "第1回大会")
+      @competition = Competition.create!(name: "第1回イベント")
       @predictions = []
     end
   end
 
   def new
-    @prediction = Prediction.new
+    @competition = Competition.find(params[:competition_id])
+    @prediction = @competition.predictions.build
     6.times { @prediction.ranking_items.build }
   end
 
   def create
-    @prediction = Prediction.new(prediction_params)
-    # 最初の大会に紐づける
-    @prediction.competition = Competition.first || Competition.create!(name: "第1回大会")
+    # フォームから送られた competition_id を使って保存先を固定
+    @competition = Competition.find(params[:prediction][:competition_id])
+    @prediction = @competition.predictions.build(prediction_params)
 
     if @prediction.save
-      redirect_to predictions_path, notice: "予想を保存しました！"
+      # 保存後、その大会のIDをURLに付けてリダイレクト（これが連動を防ぐコツ）
+      redirect_to predictions_path(competition_id: @competition.id), notice: "予想を保存しました！"
     else
       render :new, status: :unprocessable_entity
     end
@@ -32,16 +33,15 @@ class PredictionsController < ApplicationController
 
   def destroy
     @prediction = Prediction.find(params[:id])
+    comp_id = @prediction.competition_id
     @prediction.destroy
-    redirect_to predictions_path, notice: "予想を削除しました"
+    redirect_to predictions_path(competition_id: comp_id), notice: "予想を削除しました"
   end
 
   private
 
   def prediction_params
-    params.require(:prediction).permit(
-      :name,
-      ranking_items_attributes: [:id, :color_code, :rank, :_destroy]
-    )
+    # :competition_id の許可が必須です
+    params.require(:prediction).permit(:name, :competition_id, ranking_items_attributes: [:id, :color_code, :rank, :_destroy])
   end
 end
